bb3159480218d67cc5d5f818b704a12727fefbc8
AhmedTayseer50 <ahmed.tayseer600@gmail.com>
2026-03-21 20:22:27 +0200

    translation 2

--- src/app/public/consultations/consultations.component.ts
+++ src/app/public/consultations/consultations.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-consultations',
   templateUrl: './consultations.component.html',
   styleUrls: ['./consultations.component.css']
 })
-export class ConsultationsComponent {
+export class ConsultationsComponent implements OnInit {
+  currentLang: 'ar' | 'en' = 'ar';
 
+  ngOnInit(): void {
+    this.currentLang = this.detectLangFromPath();
+  }
+
+  private detectLangFromPath(): 'ar' | 'en' {
+    const seg = window.location.pathname.split('/')[1];
+    return seg === 'en' ? 'en' : 'ar';
+  }
 }
